66bf294e99462dc57dd8484915d84f641d62d1dc
Zolotopup Oleksandr <zolotopup76@gmail.com>
2023-09-05 12:36:47 +0300

    Создан класс Vehicle.h

---
+++ Garage/Vehicle.h
@@ -0,0 +1,17 @@
+#pragma once
+#include "Name.h"
+class Vehicle :
+    public Name
+{
+protected:
+    string license_plat; // гос. номер авто
+public:
+    Vehicle();
+    Vehicle(string license_plat, string name);
+
+    void set_license_plat(string license_plat);
+    string get_license_plat()const;
+
+    void show()const override;
+};
+
