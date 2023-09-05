8e010db4aebe3b45489908c1f436c9e413118249
Zolotopup Oleksandr <zolotopup76@gmail.com>
2023-09-05 11:31:19 +0300

    Создан класс Name.h

---
+++ Garage/Name.h
@@ -0,0 +1,22 @@
+#pragma once
+#include<iostream>
+#include<string>
+
+using std::cout;
+using std::endl;
+using std::string;
+
+class Name
+{
+protected:
+	string name;
+public:
+	Name();
+	Name(string name);
+
+	void set_name(string name);
+	string get_name()const;
+
+	void show()const;
+};
+
