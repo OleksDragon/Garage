a942aa9fdd8a51b41a9d75aadf413364d0217396
Zolotopup Oleksandr <zolotopup76@gmail.com>
2023-09-05 11:33:40 +0300

    Создана реализация класса Name.cpp

--- Garage/Name.cpp
+++ Garage/Name.cpp
@@ -1,1 +1,26 @@
 #include "Name.h"
+
+Name::Name()
+{
+    name = "no name";
+}
+
+Name::Name(string name)
+{
+    this->name = name;
+}
+
+void Name::set_name(string name)
+{
+    this->name = name;
+}
+
+string Name::get_name() const
+{
+    return name;
+}
+
+void Name::show() const
+{
+    cout << "Name: " << name << endl;
+}
