d692eee3ffb15415999889f2c1214c70adac5b39
Zolotopup Oleksandr <zolotopup76@gmail.com>
2023-09-05 12:41:18 +0300

    Добавлена реализация класса Vehicle.cpp

--- Garage/Vehicle.cpp
+++ Garage/Vehicle.cpp
@@ -1,1 +1,28 @@
 #include "Vehicle.h"
+
+Vehicle::Vehicle()
+{
+    license_plat = "AX 0000 AX"; // номер авто
+    name = "Mercedes Benz"; // марка авто
+}
+
+Vehicle::Vehicle(string license_plat, string name):Name(name)
+{
+    this->license_plat = license_plat;
+}
+
+void Vehicle::set_license_plat(string license_plat)
+{
+    this->license_plat = license_plat;
+}
+
+string Vehicle::get_license_plat() const
+{
+    return license_plat;
+}
+
+void Vehicle::show() const
+{
+    cout << "License plat: " << license_plat << endl;
+    cout << "Car model: " << name << endl;
+}
